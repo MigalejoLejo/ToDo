ed53c0f9b9f9a3c7864b400642dc40499dcf0df0
Miguel Alejandro Correa (PWC) <miguelalejandro.correa.avila@pwc.com>
2023-05-17 10:04:37 +0100

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Miguel Alejandro Correa Avila on 17/5/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
